6c63f60088d9234deaf2b2cd3127e1d14bfdcd60
Joe Foley <jwfoley+git@stanford.edu>
2017-06-02 13:03:43 -0700

    first commit with find_big_files.sh

---
+++ find_big_files.sh
@@ -0,0 +1,4 @@
+# find large files below the current directory and make a list of them by compression ratio, estimated from a test run of the first megabyte, so you can identify candidates for compression to save disk space
+
+find . -size +1G -exec du -0h {} \; -exec echo -ne "\t" \; -exec sh -c 'echo "1048576 / " $(dd if="$0" bs=1M count=1 2>/dev/null | gzip | wc -c) | bc' {} \; | tr -d '\000' | sort -rnk 3 -nk 1
+
