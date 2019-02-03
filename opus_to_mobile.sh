fa9ce624b7f90218801459b04e4297bee58dbdec
Joe Foley <joe@jwfoley.com>
2019-02-02 20:41:36 -0800

    tolerate spaces in filenames

--- opus_to_mobile.sh
+++ opus_to_mobile.sh
@@ -4,7 +4,7 @@
 # assumes MTP device is mounted via GVFS by the current user (standard in Ubuntu)
 # also copies album art if present
 
-ffmpeg_args='-vn -b:a 64k -ac 2 -n -nostdin' # no video, 64 kbps, force stereo, do not overwrite nor ask about it, non-interactive
+ffmpeg_args='-b:a 64k -ac 2 -n -nostdin' # no video, 64 kbps, force stereo, do not overwrite nor ask about it, non-interactive
 devicepath=/run/user/$UID/gvfs/mtp*
 outpath='Internal shared storage/Music'
 filetypes=(mp3 flac m4a mp4)
@@ -45,8 +45,8 @@ do
 		then
 			for sourcefile in "$dir"/*.$filetype
 			do
-				newfile=$(basename "$sourcefile" | sed "s/\.$filetype$/.opus/")
-				ffmpeg -i \'"$sourcefile"\' "$ffmpeg_args" \'"$devicepath/$outpath/$dirname/$newfile"\'
+				newfile=$devicepath/$outpath/$dirname/$(basename "$sourcefile" | sed "s/\.$filetype$/.opus/")
+				ffmpeg -i "$sourcefile" $ffmpeg_args "$newfile"
 			done
 		fi
 	done
