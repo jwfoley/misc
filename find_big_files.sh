2e8a7b4581729fb74bb59743bd2159f1cc47e143
Joe Foley <joe@jwfoley.com>
2019-03-04 12:59:39 -0800

    update description

--- find_big_files.sh
+++ find_big_files.sh
@@ -1,4 +1,4 @@
-# find large files below the current directory and make a list of them with date of last access and potential compression ratio, estimated from a test run of the first megabyte, so you can identify candidates for compression or deletion to save disk space
+# find large files below the current directory and make a list of them with date of last modification and potential compression ratio, estimated from a test run of the first megabyte, so you can identify candidates for compression or deletion to save disk space
 
 find . -size +1G -exec du -0 {} \; -exec echo -ne "\t" \; -exec stat --printf='%y\t' {} \; -exec bash -c 'echo $((1048576 / $(dd if="$0" bs=1M count=1 2>/dev/null | gzip | wc -c)))' {} \; | tr -d '\000'
 
